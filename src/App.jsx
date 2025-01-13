423a39c9d4f4f1828ed9cefc1d53b2f4aea02418
Gabriel P. Gonzalez <gabriel.andres.gonzalez.perez@gmail.com>
2025-01-13 20:55:46 -0300

    Hito1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Home />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
